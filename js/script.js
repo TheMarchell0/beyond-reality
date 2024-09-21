d03214ec2d56a65206c6d0a3a07a84ef3d1de156
TheMarchell0 <TheMarchell0@yandex.ru>
2024-09-21 23:53:43 +0600

    add main page markup + styling

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log(1)
+const activitiesSlider = new Swiper ('.features__slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    initialSlide: 1,
+    centeredSlides: true,
+    navigation: {
+        nextEl: '.features__slider .swiper-button-next',
+        prevEl: '.features__slider .swiper-button-prev',
+    },
+    pagination: {
+        el: '.features__slider .swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+})
