388b6dbe360b8c44dcaa2cb5579c01b205bcf006
unknown <TheMarchell0@yandex.ru>
2024-09-28 01:24:17 +0600

    add form validation / add login page

--- js/functions/formValidation.js
+++ js/functions/formValidation.js
@@ -1,6 +1,8 @@
 export function formValidation(form) {
     const inputs = form.querySelectorAll('.js-input-wrapper');
     const submitButton = form.querySelector('.js-submit-button');
+    const passwordInput = form.querySelector('.js-origin-password');
+    const repeatPasswordInput = form.querySelector('.js-repeat-password');
 
     inputs.forEach((input) => {
         const field = input.querySelector('.js-input');
@@ -32,11 +34,12 @@ export function formValidation(form) {
 
     function validateInput(input) {
         const isEmailField = input.classList.contains('js-email-input');
+        const isPasswordRepeatField = input.classList.contains('js-password-input');
         const field = input.querySelector('.js-input');
         const value = field.value.trim();
         const isEmpty = value === '';
 
-        input.classList.remove('error', 'email-error');
+        input.classList.remove('error', 'email-error', 'password-error');
 
         if (isEmpty) {
             input.classList.add('error');
@@ -45,6 +48,15 @@ export function formValidation(form) {
 
             if (isEmailField && !validateEmail(value)) {
                 input.classList.add('error', 'email-error');
+            }
+
+            if (isPasswordRepeatField && repeatPasswordInput) {
+                if (passwordInput.value !== repeatPasswordInput.value && repeatPasswordInput.classList.contains('touched')) {
+                    repeatPasswordInput.closest('.js-input-wrapper').classList.add('error', 'password-error');
+                }
+                else {
+                    repeatPasswordInput.closest('.js-input-wrapper').classList.remove('error', 'password-error');
+                }
             }
         }
     }
